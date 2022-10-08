c0d467d65adc8b2a821140eeb0d771464107eafe
Maxlego08 <maxlego08@sfr.fr>
2022-10-08 18:35:17 +0200

    :bug: Fix errors with thread

--- src/fr/maxlego08/zkoth/command/commands/CommandKothVersion.java
+++ src/fr/maxlego08/zkoth/command/commands/CommandKothVersion.java
@@ -19,7 +19,7 @@ public class CommandKothVersion extends VCommand {
 		message(sender, "§aVersion du plugin§7: §2" + main.getDescription().getVersion());
 		message(sender, "§aAuteur§7: §2Maxlego08");
 		message(sender, "§aDiscord§7: §2http://discord.groupez.dev/");
-		message(sender, "§aBuy it for §d6€§7: §2https://groupez.dev/resources/zKoth.9");
+		message(sender, "§aBuy it for §d8€§7: §2https://groupez.dev/resources/zKoth.9");
 		message(sender, "§aSponsor§7: §chttps://serveur-minecraft-vote.fr/?ref=345");
 
 		return CommandType.SUCCESS;

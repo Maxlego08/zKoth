201ab73e11cc9ef0615daefa630a5ce4cc86d98f
Maxlego08 <maxlego08@sfr.fr>
2022-12-11 12:06:17 +0100

    :bug: Fix commands

--- src/fr/maxlego08/zkoth/command/commands/CommandKothSetCapture.java
+++ src/fr/maxlego08/zkoth/command/commands/CommandKothSetCapture.java
@@ -1,5 +1,8 @@
 package fr.maxlego08.zkoth.command.commands;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 import fr.maxlego08.zkoth.ZKothPlugin;
 import fr.maxlego08.zkoth.command.VCommand;
 import fr.maxlego08.zkoth.zcore.enums.Message;
@@ -11,6 +14,7 @@ public class CommandKothSetCapture extends VCommand {
 	public CommandKothSetCapture(ZKothPlugin plugin) {
 		super(plugin);
 		this.addCompletion(0, (a, b) -> plugin.getKothManager().getKothNames());
+		this.addCompletion(1, (a, b) -> Arrays.asList(10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190).stream().map(e -> e.toString()).collect(Collectors.toList()));
 		this.setPermission(Permission.ZKOTH_SETCAPTURE);
 		this.addSubCommand("setcapture", "setcap");
 		this.setDescription(Message.DESCRIPTION_CAPTURE);

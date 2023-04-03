9d8c60c31c497fa2b24a925c0002296eeb2aa0c7
Maxlego08 <maxlego08@sfr.fr>
2023-04-03 13:09:30 +0200

    :memo: Fix gang and add better team

--- src/fr/maxlego08/zkoth/hooks/GangsHook.java
+++ src/fr/maxlego08/zkoth/hooks/GangsHook.java
@@ -1,12 +1,14 @@
 package fr.maxlego08.zkoth.hooks;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.bukkit.entity.Player;
 
 import fr.maxlego08.zkoth.api.FactionListener;
 import net.brcdev.gangs.GangsPlugin;
+import net.brcdev.gangs.gang.Gang;
 
 public class GangsHook implements FactionListener {
 
@@ -15,13 +17,14 @@ public class GangsHook implements FactionListener {
 
 	@Override
 	public String getFactionTag(Player player) {
-		return GangsPlugin.getInstance().getGangManager().getPlayersGang(player).getName();
+		Gang gang = GangsPlugin.getInstance().getGangManager().getPlayersGang(player);
+		return gang != null ? gang.getName() : player.getName();
 	}
 
 	@Override
 	public List<Player> getOnlinePlayer(Player player) {
-		return new ArrayList<Player>(
-				GangsPlugin.getInstance().getGangManager().getPlayersGang(player).getOnlineMembers());
+		Gang gang = GangsPlugin.getInstance().getGangManager().getPlayersGang(player);
+		return gang != null ? new ArrayList<>(gang.getOnlineMembers()) : Arrays.asList(player);
 	}
 
 }
